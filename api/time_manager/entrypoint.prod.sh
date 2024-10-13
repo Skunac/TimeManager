51733b2be83b1b0155e1afa23e439e46dd678e91
Duchesne <antoine.duchesne@epitech.eu>
2024-10-13 13:46:33 +0200

    updated ci/cd for production deployment

---
+++ api/time_manager/entrypoint.prod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Entrypoint script is running"
+
+# Wait for Postgres to become available
+until pg_isready -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "Waiting for postgres..."
+  sleep 5
+done
+
+echo "Postgres is available"
+
+# Run migrations
+/app/bin/time_manager eval "TimeManager.Release.migrate"
+
+# Start the Phoenix server
+exec /app/bin/time_manager start
---
+++ client/entrypoint.prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+echo "Entrypoint script is running"
+
+# Start the Nuxt server
+exec npm start
