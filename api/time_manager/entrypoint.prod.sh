6b66a078d9e207326a88053b4613e8663f5a85d3
Duchesne <antoine.duchesne@epitech.eu>
2024-10-13 14:54:46 +0200

    updated prod config for secret_key_base generation

--- api/time_manager/entrypoint.prod.sh
+++ api/time_manager/entrypoint.prod.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "Entrypoint script is running"
 
+# Generate SECRET_KEY_BASE if not set
+if [ -z "$SECRET_KEY_BASE" ]; then
+    export SECRET_KEY_BASE=$(mix phx.gen.secret)
+    echo "Generated new SECRET_KEY_BASE"
+fi
+
 # Wait for Postgres to become available
 until pg_isready -h $PGHOST -p $PGPORT -U $PGUSER
 do
