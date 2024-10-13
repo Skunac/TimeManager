c6562044a8b4959bdc8aab1a113a1389102aec07
Duchesne <antoine.duchesne@epitech.eu>
2024-10-13 15:05:47 +0200

    updated prod config for secret_key_base generation

--- api/time_manager/entrypoint.prod.sh
+++ api/time_manager/entrypoint.prod.sh
@@ -4,12 +4,17 @@ set -e
 echo "Entrypoint script is running"
 
 # Path to store the SECRET_KEY_BASE
-SECRET_FILE="/app/secret_key_base/key"
+SECRET_FILE="/app/secret_key_base/secret_key_base"
+
+# Function to generate a random string
+generate_secret() {
+    openssl rand -base64 48 | tr -d "\n"
+}
 
 # Generate SECRET_KEY_BASE if not set and not stored
 if [ -z "$SECRET_KEY_BASE" ]; then
     if [ ! -f "$SECRET_FILE" ]; then
-        mix phx.gen.secret > "$SECRET_FILE"
+        generate_secret > "$SECRET_FILE"
         echo "Generated new SECRET_KEY_BASE and stored it"
     fi
     export SECRET_KEY_BASE=$(cat "$SECRET_FILE")
