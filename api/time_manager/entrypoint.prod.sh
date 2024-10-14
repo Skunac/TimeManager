b341ca714a737a501d0e63b1a269e35eae36d22d
Duchesne <antoine.duchesne@epitech.eu>
2024-10-14 16:02:22 +0200

    small fixes to docker config

--- api/time_manager/entrypoint.prod.sh
+++ api/time_manager/entrypoint.prod.sh
@@ -21,14 +21,6 @@ if [ -z "$SECRET_KEY_BASE" ]; then
     echo "Loaded SECRET_KEY_BASE from file"
 fi
 
-# Ensure all required environment variables are set
-: "${PGUSER:?PGUSER must be set}"
-: "${PGPASSWORD:?PGPASSWORD must be set}"
-: "${PGDATABASE:?PGDATABASE must be set}"
-: "${PGHOST:?PGHOST must be set}"
-: "${PGPORT:=5432}"
-: "${POOL_SIZE:=10}"
-
 # Wait for Postgres to become available
 until pg_isready -h $PGHOST -p $PGPORT -U $PGUSER
 do
