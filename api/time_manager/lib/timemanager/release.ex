779774949297e24e3d4b08d2ea7304b2c0bab24e
Duchesne <antoine.duchesne@epitech.eu>
2024-10-15 10:14:40 +0200

    updated docker config

--- api/time_manager/lib/timemanager/release.ex
+++ api/time_manager/lib/timemanager/release.ex
@@ -1,5 +1,8 @@
-# lib/timemanager/release.ex
 defmodule Timemanager.Release do
+  @moduledoc """
+  Used for executing DB release tasks when run in production without Mix
+  installed.
+  """
   @app :timemanager
 
   def migrate do
@@ -13,32 +16,6 @@ defmodule Timemanager.Release do
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
-  end
-
-  def create_db do
-    load_app()
-
-    for repo <- repos() do
-      :ok = ensure_repo_created(repo)
-    end
-  end
-
-  defp ensure_repo_created(repo) do
-    IO.puts("Create #{inspect(repo)} database if it doesn't exist")
-
-    config = Keyword.merge(repo.config,
-      username: System.get_env("PGUSER"),
-      password: System.get_env("PGPASSWORD"),
-      database: System.get_env("PGDATABASE"),
-      hostname: System.get_env("PGHOST"),
-      port: String.to_integer(System.get_env("PGPORT") || "5432")
-    )
-
-    case repo.__adapter__.storage_up(config) do
-      :ok -> :ok
-      {:error, :already_up} -> :ok
-      {:error, term} -> {:error, term}
-    end
   end
 
   defp repos do
